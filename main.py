7112bd19c43945340078c3e25e0332e8c60c24f8
AdikHD <adiarmyrzakanov5@gmail.com>
2026-09-03 14:02:33 +0600

    Update model to use 'gemini-pro' version

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 
 genai.configure(api_key=GEMINI_API_KEY)
 # Используем самую актуальную версию Flash
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
+
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
